48bc51719c02c5400e43a5d40e946a206a805e87
Urfhjn <rjguevara98@gmail.com>
2021-04-26 11:42:06 +0800

    Edit files for basic template

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        Site footer placeholder
+    </footer>
+    
+    <?php wp_footer() ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Awesome Theme</title>
-</head>
-<body>
-    <header>
-        This is Header
-    </header>
+<?php get_header() ?>
 
     <main>
-        Content here.
+        <!-- Wordpress Loop -->
+        <?php if (have_posts()) : while(have_posts()) : the_post() ?>
+            <div class="post-header">
+                <h2><?php the_title() ?></h2>
+            </div>
+            <div class="post-content">
+                <?php the_content() ?>
+            </div>
+        <?php endwhile; endif; ?>
     </main>
 
-    <footer>
-        Site footer placeholder
-    </footer>
-    
-</body>
-</html>
+<?php get_footer() ?>
